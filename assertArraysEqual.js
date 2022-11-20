3d63cc207c22af923ccdc93408d654aa9b3aadd9
Andre <andrepaesbm@gmail.com>
2022-11-20 11:35:42 -0700

    Created a function to compare 2 arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    console.log(`✅ Test Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ Test Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 9, 2], [1, 4, 8]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 10]);
