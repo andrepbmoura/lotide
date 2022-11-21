9cf9a14ff8559cf6d9482713125dce8cd4b7072d
Andre <andrepaesbm@gmail.com>
2022-11-20 20:29:12 -0700

    created a function to find the middle of an array

---
+++ middle.js
@@ -0,0 +1,29 @@
+const eqArrays = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    console.log(`✅ Test Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ Test Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  let mid = 0;
+  for (let i = 0; i < array.length; i++) {
+    mid = array[0] + (array.slice(-1) - array[0]) / 2;
+  }
+  return Math.floor(mid);
+};
+
+console.log(middle([1, 2, 3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4]), true));
+
+
+
