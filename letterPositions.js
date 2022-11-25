898e748ae09f403b296f442fbb30673d3e3ed563
Andre <andrepaesbm@gmail.com>
2022-11-24 21:04:56 -0700

    Did some improvements in the repository in general

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,8 @@
 const assertArraysEqual = function(array1, array2) {
   if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    console.log(`✅ Test Passed: ${array1} === ${array2}`);
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`❌ Test Failed: ${array1} !== ${array2}`);
+    console.log(`❌ Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
