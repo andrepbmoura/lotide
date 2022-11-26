b7a50e93ec5517fd40ccb1632508a24d532080a1
Andre <andrepaesbm@gmail.com>
2022-11-26 15:21:45 -0700

    created a function that stops to loop in the array whenever the callback returned a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,33 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+  return results;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
