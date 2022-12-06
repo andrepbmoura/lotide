71d9fc995ead9fb8556cc3dea0e305c388be35fb
Andre <andrepaesbm@gmail.com>
2022-12-06 12:18:41 -0700

    refactored the tailTest file to test the function with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,18 @@
+const assert = require('chai').assert
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse"); 
-assertEqual(result[1], "Labs");
+
+describe("should return the 'tail' of an array", () => {
+  it("returns the length of the array without the first element", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result.length, 2);
+  });
+  it("returns 'Lighthouse'", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result[0], "Lighthouse");
+  });
+  it("returns 'Labs'", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result[1], "Labs");
+  });
+});
