f399255f8b51279317442b4c567ec4476370b3b1
Andre <andrepaesbm@gmail.com>
2022-12-06 12:19:41 -0700

    did some changes into the middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,10 +2,6 @@ const assert = require('chai').assert
 const middle = require('../middle')
 
 
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); => returns 2, 3
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-
 describe("should return the middle of an array", () => {
   it("returns '2, 3' for '1, 2, 3, 4'", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
