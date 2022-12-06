74c4ddef0318de89feb0e430c1815c0b54ab1928
Andre <andrepaesbm@gmail.com>
2022-12-05 21:54:09 -0700

    added a test file for the middle function and did some refactoring as well

--- middle.js
+++ middle.js
@@ -1,34 +1,18 @@
-const eqArrays = function(array1, array2) {
-  if (JSON.stringify(array1) !== JSON.stringify(array2)) {
-    return false;
-  } else {
-    for (const i of array1) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (JSON.stringify(array1) === JSON.stringify(array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
-  let mid = 0;
-  for (let i = 0; i < array.length; i++) {
-    mid = array[0] + (array.slice(-1) - array[0]) / 2;
+  let mid = [];
+  let midArray = array.length / 2;
+  if (array.length % 2 === 0) {
+    mid.push(array[midArray - 1], array[midArray]);
+  } else if (array.length === 1 || array.length === 2) {
+    return array;
+  } else {
+    mid.push(array[Math.floor(midArray)]);
   }
-  return Math.floor(mid);
+  return mid;
 };
 
-console.log(middle([1, 2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), true));
+module.exports = middle;
+
 
 
 
