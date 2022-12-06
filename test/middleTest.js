d539b839e9359fb84bcaf5d2d34bca2995b5dfac
Andre <andrepaesbm@gmail.com>
2022-12-06 12:18:11 -0700

    refactored the middleTest file to test the function with Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert
 const middle = require('../middle')
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); => returns 2, 3
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("should return the middle of an array", () => {
+  it("returns '2, 3' for '1, 2, 3, 4'", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns '3, 4' for '1, 2, 3, 4, 5, 6'", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
